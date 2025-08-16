cef0adc4b6dcb61aeb41388bcf9e6acce5a060d2
Nahian117 <blacktshirt409@gmail.com>
2025-08-16 18:06:14 +0600

    Add files via upload

---
+++ DBConnect.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username   = "root";
+$password   = "";
+$DBname     = "TMS";
+
+$Checker = new mysqli($servername, $username, $password, $DBname);
+
+if ($Checker->connect_error) {
+    die("Connection Failed: " . $Checker->connect_error);
+} else {
+    echo "Connection Success!";
+}
+?>
