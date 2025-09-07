7e4ccbd277e3436bf10376ca0f1d6ca4dfa91059
Nahian117 <blacktshirt409@gmail.com>
2025-09-08 01:11:40 +0600

    Add files via upload

---
+++ SignIn.php
@@ -0,0 +1,17 @@
+<?php
+require_once("DBConnect.php");
+if (isset($_POST["username"]) && isset($_POST["password"])){
+	$u = $_POST["username"];
+	$p = $_POST["password"];
+	$sql = "select * from user where name = '$u' and password = '$p'";
+	$result = mysqli_query($conn,$sql);
+	if (mysqli_num_rows($result) !=0){
+		header("Location: home.php");
+		//echo "Login successful";
+	}
+	else{
+		echo "Invalid username and passoword";
+	}
+
+}
+?>
